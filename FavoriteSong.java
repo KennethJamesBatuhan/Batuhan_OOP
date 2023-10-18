f6846d8b6a14544949261d562d4774efff1f72b1
KennethJames <kennethjamesbatuhan@gmail.com>
2023-10-18 23:30:28 +0800

    K

---
+++ FavoriteSong.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class FavoriteSong{
+   public static void main(String[] args){
+
+   System.out.println("Life can be short or long");
+   System.out.println("Love can be right or wrong");
+   System.out.println("And if I chose the one I'd like to help me through");
+   System.out.println("I'd like to make it with you");
+
+   }
+}
